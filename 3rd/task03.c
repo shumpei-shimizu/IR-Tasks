561b9f7849d979bbea63511ecfab0586cf33f249
Shumpei Shimizu <irvine.shumpei.shimizu@gmail.com>
2021-11-22 18:12:53 +0900

    complete task 03

---
+++ 3rd/task03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int double_counter(int num) {
+	if (num == 0) return 0;
+	return double_counter(num - 1) + num * num;
+}
+
+int main(void) {
+	printf("%d", double_counter(100));
+	return 0;
+}
+
---
+++ 3rd/task03.js
@@ -0,0 +1,7 @@
+function double_counter(num) {
+	if (num == 0) return 0;
+	return double_counter(num - 1) + num ** 2;
+}
+
+console.log(double_counter(100));
+
