77180c04e0526fe77b4d765304d4035e48998e94
Shumpei Shimizu <irvine.shumpei.shimizu@gmail.com>
2021-11-26 12:39:34 +0900

    refact task02

--- 2nd/task02.py
+++ 2nd/task02.py
@@ -1,5 +1,5 @@
 sum = 0
 for i in range(100) :
-    sum += (i+1)**2
+    sum += (i + 1) ** 2
 print(sum)
 
---
+++ 2nd/task02_another.py
@@ -0,0 +1,5 @@
+def jijowa(num) :
+    return num * (num + 1) * (num * 2 + 1) / 6
+
+print(jijowa(100))
+
